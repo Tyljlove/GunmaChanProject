2787c8c4aea9938ef1b69369918164d908a646d0
SqueetleTB <39778430+SqueetleTB@users.noreply.github.com>
2018-10-27 17:37:44 -0700

    "Initial" commit, now with LibGDX
    All the base project files to allow Android Studio to use LibGDX libraries.
    
    Additionally, GunmaChan.java is the "main" class at the moment, and DesktopLauncher.java has been tweaked to work with it. "super_gunma.png" is provided as a temporary asset as well.

---
+++ desktop/src/com/gunmachan/game/desktop/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package com.gunmachan.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import asu.gunma.GunmaChan;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		// This creates Windowed Borderless mode
+		//System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+		config.width = GunmaChan.WIDTH;
+		config.height = GunmaChan.HEIGHT;
+		config.title = GunmaChan.TITLE;
+		// Setting this to true creates Fullscreen mode
+		//config.fullscreen = false;
+		new LwjglApplication(new GunmaChan(), config);
+	}
+}
